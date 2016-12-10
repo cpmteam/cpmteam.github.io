52234f303f4b3103f745658393acecae9892d38d
Dmitry Maslennikov <mrdaimor@gmail.com>
2016-12-10 22:34:55 +0300

    new way for webpack

--- src/app.js
+++ src/app.js
@@ -1,6 +1,29 @@
 'use strict';
 
-var app = angular.module('sampleApp', ['ngRoute', 'ngMaterial', 'ui.bootstrap','hc.marked']);
+/// Styles loading
+import 'angular-material/angular-material.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'material-design-icons/iconfont/material-icons.css';
+import 'source-sans-pro/source-sans-pro.css';
+import './css/default.css';
+
+window.$ = window.jQuery = require('jquery');
+import 'typed.js';
+
+import angular from 'angular';
+
+import 'angular-material';
+import 'angular-route';
+import 'angular-marked';
+// import 'bootstrap';
+import 'angular-bootstrap';
+
+let app = angular.module('app', [
+	'ngRoute',
+	'ngMaterial',
+	'ui.bootstrap',
+	'hc.marked'
+]);
 
 app.config(function($mdThemingProvider) {
   $mdThemingProvider.theme('default')
@@ -16,13 +39,29 @@ app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 		.when('/', 
 		{
-			templateUrl: 'partial/main-flex.html',
+			template: require('./templates/main-flex.html'),
 			controller: 'MainCtrl',
 		})
 		.when('/repo/:name', 
 		{
-			templateUrl: 'partial/repo.html', 
+			template: require('./templates/repo.html'), 
 			controller: 'RepoCtrl',
 		})
 		.otherwise({redirectTo: '/'});
 }]);
+
+// Services
+let services = require.context('./services', true, /.js$/);
+services.keys().forEach(services);
+ 
+// Controllers
+let controllers = require.context('./controllers', true, /.js$/);
+controllers.keys().forEach(controllers);
+
+$("#typed").typed({
+		strings: ["Caché", "Community"],
+		typeSpeed: 30,
+		backDelay: 1500,
+		loop: false,
+		loopCount: 3
+	});
