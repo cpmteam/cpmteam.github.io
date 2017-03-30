dc7e19b53cf55675648c1e6fbf6ae1014e0d7a73
Timur Safin <timur.safin@gmail.com>
2017-03-31 02:55:36 +0300

    Automagically convert names like `user~repo-name` into `user/repo-name` for display purposes

--- src/controllers/mainCtrl.js
+++ src/controllers/mainCtrl.js
@@ -1,33 +1,48 @@
-angular.module('app').controller('MainCtrl', ['$scope', '$http', 'DataSrvc', '$location', function($scope, $http, DataSrvc, $location){
-  $scope.data = [];
-  $scope.sourceData = [];
-  $scope.numLimit = 3;
-
-  DataSrvc.getData(function(data) {
-    var localData = data.data;
-    for (let item in localData) {
-      if (item != '_updated'){
-        localData[item].latest = localData[item]['dist-tags'].latest
-        $scope.sourceData.push(localData[item]);
-      }
-    }
-    $scope.data= $scope.sourceData
-  })
-
-  $scope.locate = function(path){
-    $location.path(path)
-  };
-
-  $scope.enter = function(term){
-    if (!term){
-      $scope.data = $scope.sourceData
-      return;
-    }
-
+angular.module('app').controller('MainCtrl', ['$scope', '$http', 'DataSrvc', '$location', function($scope, $http, DataSrvc, $location) {
     $scope.data = [];
-    $scope.sourceData.forEach(function(item){
-      if(item.name.indexOf(term) >= 0)
-        $scope.data.push(item);
+    $scope.sourceData = [];
+    $scope.numLimit = 3;
+
+
+    function getPurePackageName(packageName) {
+        var items = packageName.split('~');
+        var head = items.shift();
+        return head + '/' + items.join('~');
+    }
+
+    function getShortPackageName(packageName) {
+        var items = packageName.split('~');
+        var head = items.shift();
+        return items.join('~');
+    }
+
+    DataSrvc.getData(function(data) {
+        var localData = data.data;
+        for (let item in localData) {
+            if (item != '_updated') {
+                localData[item].latest = localData[item]['dist-tags'].latest
+                localData[item].nname = getPurePackageName(item);
+                localData[item].sname = getShortPackageName(item);
+                $scope.sourceData.push(localData[item]);
+            }
+        }
+        $scope.data = $scope.sourceData
     })
-  };
+
+    $scope.locate = function(path) {
+        $location.path(path)
+    };
+
+    $scope.enter = function(term) {
+        if (!term) {
+            $scope.data = $scope.sourceData
+            return;
+        }
+
+        $scope.data = [];
+        $scope.sourceData.forEach(function(item) {
+            if (item.name.indexOf(term) >= 0)
+                $scope.data.push(item);
+        })
+    };
 }]);
